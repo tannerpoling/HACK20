9e2ac54ce188a604f1da4e2f5c9c8f25d28ad70e
Patrick Render <rendep@uw.edu>
2020-08-15 14:13:26 -0700

    added project folder

--- monumental_change/ios/Runner/AppDelegate.swift
+++ monumental_change/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyATI5JvYvomCjqC9BURRu8Ly-9w6yILXBs")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
